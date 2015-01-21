65e2c839b4d537298906b1b4a78725d681ab3c8a
Chris Tippett <c.tippett@gmail.com>
2015-01-21 22:01:14 +1100

    Chapter 3 complete. Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
-from lists.views import home_page
+from django.http import HttpRequest
+
+# Import view function
+from lists.views import home_page # home_page is the view function stored in lists/views.py
 
 # TestCase is an augmented version of the standard unittest.TestCase,
 # with some Django-specific features thrown in
@@ -16,4 +19,15 @@ class HomePageTest(TestCase):
 		found = resolve('/') # function Django uses internally to resolve URLs and find what view function they should be mapped to
 		self.assertEqual(found.func, home_page) # Check that resolve, when called with '/', finds a function called home_page
 		
-		# NB: home_page is the view function stored in lists/views.py
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest() # An HttpRequest object, this is what Django sees when a user's browser asks for a page
+		response = home_page(request) # We pass this request to our home_page view, which gives a reponse in form of an HttpResponse object
+		
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
+		
+		# NB: response.content is the HTML contents of the HttpResponse object
+		
+		# NB: response.content is in raw bytes, not a Python string, so
+		# we have to use the b'' syntax to compare them
