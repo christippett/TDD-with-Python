3dd1b528f80cb128aa60ad69455fa8ebd4bf3130
Chris Tippett <c.tippett@gmail.com>
2015-01-28 22:01:37 +1100

    Chapter 7 in-progress. Added Bootstrap styles to app.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # import unittest
@@ -8,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 # documentation for more information
 
 
-class NewVisitorTest(LiveServerTestCase): # Tests are organised into classes, which inherit from unittest.TestCase
+class NewVisitorTest(StaticLiveServerTestCase): # Tests are organised into classes, which inherit from unittest.TestCase
 	
 	def setUp(self): # This method runs before each test
 		self.browser = webdriver.Firefox()
@@ -116,6 +117,18 @@ class NewVisitorTest(LiveServerTestCase): # Tests are organised into classes, wh
 		
 		# Satisfied, they both go back to sleep
 
+	def test_layout_and_styling(self):
+		# Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# She notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
 
 # No longer needed since we're using Django's LiveServerTestCase
 # if __name__ == '__main__': # Is this script being run from the command-line?
