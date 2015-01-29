29dc4d3c54ea4701f2afd0b7b6c5e92cba6e1eed
Chris Tippett <c.tippett@gmail.com>
2015-01-29 14:53:56 +1100

    Add static files

--- lists/tests.py
+++ lists/tests.py
@@ -27,7 +27,7 @@ class HomePageTest(TestCase):
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
 		
-		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
 		self.assertIn(b'<title>To-Do lists</title>', response.content)
 		self.assertTrue(response.content.endswith(b'</html>'))
 		
