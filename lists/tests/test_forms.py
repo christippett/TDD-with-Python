9c2e3d503d62af32c23018ca387b8820c6b1bbf0
Chris Tippett <c.tippett@gmail.com>
2015-02-15 16:37:05 +1100

    Chapter 12. Added form validation for existing lists, check for duplicate list items.

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -29,12 +29,12 @@ class ItemFormTest(TestCase):
 class ExistingListItemFormTest(TestCase):
 
 	def test_form_renders_item_text_input(self):
-		list_ = Item.objects.create()
+		list_ = List.objects.create()
 		form = ExistingListItemForm(for_list=list_)
 		self.assertIn('placeholder="Enter a to-do item"', form.as_p())
 
 	def test_form_validation_for_blank_items(self):
-		list_ = Item.objects.create()
+		list_ = List.objects.create()
 		form = ExistingListItemForm(for_list=list_, data={'text': ''})
 		self.assertFalse(form.is_valid())
 		self.assertEqual(
@@ -42,10 +42,9 @@ class ExistingListItemFormTest(TestCase):
 			[EMPTY_ITEM_ERROR]
 		)
 
-	def test_form_save_handles_saving_to_a_list(self):
+	def test_form_validation_for_duplicate_items(self):
 		list_ = List.objects.create()
-		form = ItemForm(data={'text': 'do me'})
-		new_item = form.save(for_list=list_)
-		self.assertEqual(new_item, Item.objects.first())
-		self.assertEqual(new_item.text, 'do me')
-		self.assertEqual(new_item.list, list_)
+		Item.objects.create(list=list_, text='no twins!')
+		form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
